a01ce8f13d9b5c46144264831afaa5bb95706b32
Payal Sharma <payalsharmareal@gmail.com>
2023-12-25 18:02:37 +0530

    place route for updates

--- ToDo/routes/placeRoutes.js
+++ ToDo/routes/placeRoutes.js
@@ -4,5 +4,6 @@ const{ placeInfo} = require('./../controllers/placeController');
 const router = express.Router();
 
 router.post('/places', placeInfo);
+router.put('/places/update', updatePlace);
 
 module.exports = router;
